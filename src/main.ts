ca9c8da61b280bd48b3bb142da995d6e3d9c1116
Délita Makanda <delita.makanda@gmail.com>
2025-07-23 20:11:12 +0200

    add i18n plugin

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import './style.css'
 import App from './App.vue'
 import { router } from './router'
 import { store } from './store'
+import { i18n } from './i18n'
 
 createApp(App)
     .use(router)
     .use(store)
+    .use(i18n)
     .mount('#app')
